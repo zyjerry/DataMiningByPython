f67fa4c6d3a753451e35a4102e0b1e0ddecb523e
Jerry Zhang <44934998+zyjerry@users.noreply.github.com>
2019-09-07 17:09:12 +0800

    Update DecisionTree.py
    增加注释，完善了细节问题

--- main/SKLearnModels/DecisionTree.py
+++ main/SKLearnModels/DecisionTree.py
@@ -2,8 +2,8 @@
     0.前言：
     数据挖掘项目最繁琐且最不可自动化的环节在前期数据清洗，所以本程序不做演示和赘述。
     已经有一份处理好且自变量均有效的宽表数据dd_df.xlsx（见本目录下SAS数据文件）：
-    loan_id为流水号，y是因变量（其中y=1表示坏客户），其余均为自变量。
-    本程序着重演示如何使用决策树将连续数据离散化为最理想状态，并建模并展示模型效果。
+    loan_id为流水号，y是因变量（二分类，其中y=1表示坏客户），其余均为自变量。
+    本程序着重演示如何使用决策树方法建模，并评估模型。
 """
 import pandas
 import sklearn.tree
@@ -22,16 +22,29 @@ X_train, X_test, y_train, y_test = \
                                              random_state=0)
 
 """
-    2. 开始决策树建模
+    2. 开始决策树建模，主要（非全部）参数介绍：
+        criterion：衡量节点“不纯度”的标准，默认gini，可选entropy信息增益；
+        splitter：选择每个节点的分枝策略。
+            默认best，表示根据criterion的最优表现选择自变量，需大量计算，一般用于训练样本数据量不大的场合
+            可选random，表示随机划分属性，一般用于训练数据量较大的场合，可以减少计算量
+        max_depth：设置决策树的最大深度，默认为None。None表示不对决策树的最大深度作约束，直到每个叶子结点上
+            的样本均属于同一类，或者少于min_samples_leaf参数指定的叶子结点上的样本个数。一般不建议指定深度，
+            过拟合或是降低复杂度的问题，还是通过剪枝来改善比较有效。
+        min_samples_split：当对一个内部结点划分时，结点上的最小样本数，默认为2。
+        min_samples_leaf：设置叶子结点上的最小样本数，默认为1。
+        max_leaf_nodes: 设置决策树的最大叶子节点个数，默认为None，表示不加限制。
+        min_impurity_decrease :打算划分一个内部结点时，只有当划分后不纯度减少值不小于该参数指定的值，才会对该结点进行划分，默认值为0。
+        class_weight：设置样本数据中每个类的权重，默认为None，用户可以用字典型数据指定每个类的权重。
+            该参数还可以设置为‘balance’，此时系统会按照输入的样本数据自动的计算每个类的权重，属于某个类的样本个数越多时，该类的权重越小。
 """
-dtc = sklearn.tree.DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None,
-                                          min_samples_split=50, min_samples_leaf=10)
+dtc = sklearn.tree.DecisionTreeClassifier(min_samples_split=100, min_samples_leaf=50)
 dtc.fit(X_train, y_train)
 print('Train score:{:.3f}'.format(dtc.score(X_train, y_train)))
 print('Test score:{:.3f}'.format(dtc.score(X_test, y_test)))
 print('classes_:', dtc.classes_)
-print('feature_importances_:', dtc.feature_importances_)
-# print('tree_:', dtc.tree_)
+useful_columns_df = pandas.DataFrame(useful_columns, columns=['column_name'])
+useful_columns_df['feature_importance'] = dtc.feature_importances_
+print(useful_columns_df)
 
 """
     3. 生成可视化图
@@ -42,3 +55,7 @@ sklearn.tree.export_graphviz(dtc, out_file='tree.dot', feature_names=useful_colu
 graph = pydotplus.graph_from_dot_file('tree.dot')
 graph.write_pdf('tree.pdf')
 print('Visible tree plot saved as pdf.')
+
+"""
+    4. 生成可视化图之后，需要再肉眼查看一下，可以做进一步的剪枝策略
+"""
